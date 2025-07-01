af8b056a617226d3e13e7c2b68ba1e92d5b74b1f
JerryLmr <jerrylmr@icloud.com>
2025-07-02 01:55:04 +0800

    完成日志系统

--- log.h
+++ log.h
@@ -2,23 +2,35 @@
 #define __SYLAR_LOG_H
 
 #include <cstdint>
-#include <fstream>
 #include <string>
 #include <stdint.h>
 #include <memory>
 #include <list>
-#include <strstream>
 #include <fstream>
+#include <sstream>
 #include <vector>
-
 
 namespace sylar {
 
+class Logger;
 //日志事件
 class LogEvent {
 public:
     typedef std::shared_ptr<LogEvent> ptr;
-    LogEvent();
+    LogEvent(const char* file, int32_t line, uint32_t elapse
+            , uint32_t thread_id, uint32_t fiber_id, uint64_t time);
+
+    const char* getFile() const {return m_file;}
+    int32_t getLine() const {return m_line;}
+    uint32_t getElapse() const {return m_elapse;}
+    uint32_t getThreadId() const {return m_threadId;}
+    uint32_t getFiberId() const {return m_fiberId;}
+    uint64_t getTime() const {return m_time;}
+    std::string getContent() const {return m_ss.str();}
+
+    const std::stringstream& getSS() const { return m_ss; }
+    std::stringstream& getSS() { return m_ss; }
+
 private:
     const char* m_file = nullptr; //文件名
     int32_t m_line = 0;           //行号
@@ -26,19 +38,22 @@ private:
     uint32_t m_threadId = 0;      //线程id
     uint32_t m_fiberId = 0;       //协程id  
     uint64_t m_time;              //时间戳
-    std::string m_content;
+    std::stringstream m_ss;
 };
 
 //日志级别
 class LogLevel {
 public:
     enum Level{
+        UNKNOW = 0,
         DEBUG = 1,
         INFO = 2,
         WARN = 3,
         ERROR = 4,
         FATAL = 5
     };
+
+    static const char* ToString(LogLevel::Level level);
 };
 
 //日志格式器
@@ -48,15 +63,16 @@ public:
     LogFormatter(const std::string& pattern);
     
     //%t   %thread)id %m %n
-    std::string format(LogEvent::ptr event);
-private:
+    std::string format(std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::ptr event);
+public:
     class FormatItem {
     public:
         typedef std::shared_ptr<FormatItem> ptr;
+        //FormatItem(const std::string& fmt = "") {}
         virtual ~FormatItem() {}
-        virtual std::string format(LogEvent::ptr event) = 0;
+        virtual void format(std::ostream& os, std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::ptr event) = 0;
     };
-
+    void init();
 private:
     std::string m_pattern;
     std::vector<FormatItem::ptr> m_items;
@@ -68,7 +84,7 @@ public:
     typedef std::shared_ptr<LogAppender> ptr;
     virtual ~LogAppender() {}
 
-    virtual void log(LogLevel::Level level, LogEvent::ptr event) = 0;
+    virtual void log(std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::ptr event) = 0;
 
     void setFormatter(LogFormatter::ptr val) {m_formatter = val;}
     LogFormatter::ptr getFormater() const {return m_formatter;}
@@ -78,7 +94,7 @@ protected:
 };
 
 //日志器
-class Logger {
+class Logger : public std::enable_shared_from_this<Logger>{
 public:
     typedef std::shared_ptr<Logger> ptr;
 
@@ -95,24 +111,27 @@ public:
     void delAppender(LogAppender::ptr appender);
     LogLevel::Level getLevel() const {return m_level;}
     void setLevel(LogLevel::Level val) {m_level = val;}
+
+    const std::string& getName() const {return m_name;}
 private:
     std::string m_name;                     //日志名称
     LogLevel::Level m_level;                //日志级别
     std::list<LogAppender::ptr> m_appenders;//Appender集合
+    LogFormatter::ptr m_formatter;
 };
 
 //输出到控制台的Appender
 class StdoutLogAppender : public LogAppender{
 public:
     typedef std::shared_ptr<StdoutLogAppender> ptr;
-    void log(LogLevel::Level level, LogEvent::ptr event) override;
+    void log(Logger::ptr logger, LogLevel::Level level, LogEvent::ptr event) override;
 private:
 };
 //定义输出到文件的Appender
 class FileLogAppender : public LogAppender {
     typedef std::shared_ptr<FileLogAppender> ptr;
     FileLogAppender(const std::string& filename);
-    void log(LogLevel::Level level, LogEvent::ptr event) override;
+    void log(Logger::ptr logger, LogLevel::Level level, LogEvent::ptr event) override;
 
     //重新打开文件，文件打开成功返回true
     bool reopen();
